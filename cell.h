4bd3493a7c866b95fced00888d71d544345af715
jeanne-lcr <jeanne.leclere@efrei.net>
2025-10-20 15:09:56 +0200

    structures cell and list

--- cell.h
+++ cell.h
@@ -4,5 +4,11 @@
 
 #ifndef CELL_H
 #define CELL_H
+typedef struct cell {
+  int arrival;
+  float probability;
+  struct cell *next;
+} cell;
+
 
 #endif //CELL_H
--- list.h
+++ list.h
@@ -1,8 +1,13 @@
-//
-// Created by edoua on 20/10/2025.
-//
+#include "cell.h"
 
 #ifndef LIST_H
 #define LIST_H
+typedef struct list {
+  cell *head;
+}list;
 
+typedef struct AdjacencyList{
+  int size;
+  list *list;
+}AdjacencyList;
 #endif //LIST_H
